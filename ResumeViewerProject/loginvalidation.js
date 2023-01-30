50f104a7e487db3f15644608ee3c0a8dc07f521a
Sarvani-Andugulapati <105815558+Sarvani-Andugulapati@users.noreply.github.com>
2023-01-30 23:22:47 +0530

    Final Code

---
+++ ResumeViewerProject/loginvalidation.js
@@ -0,0 +1,53 @@
+const Credentials = [
+    {
+        username: "John Doe",
+        password: "John2023!@#"
+    },
+    {
+        username: "Smitha Singh",
+        password: "Smit2023!@#"
+    },
+    {
+        username: "Rahul Rana",
+        password: "Rahul2023!@#"
+    }
+]
+
+window.localStorage.setItem("Credentials", JSON.stringify(Credentials));
+console.log(Credentials);
+
+var userName = document.getElementById('Username');
+ var userPw = document.getElementById('password');
+
+    function validate() {
+        console.log(userName.value,userPw.value);
+        if(userName.value.length!== 0 && userPw.value.length !==0){
+            login();
+        }else{
+            alert("Please enter Username and Password");
+        }
+    }
+    function login(){
+        const storedData = JSON.parse(localStorage.getItem('Credentials'));
+        if(storedData.length!==0){
+            const userdetails = storedData.find((user)=>{
+                if(userName.value === user.username){
+                    return user;
+                }else{
+                    alert("Invalid Username/Password");
+                }
+            });
+            if(userPw.value === userdetails.password){
+                alert("You are succesfully logged in");
+                window.location.href = "./ResumeViewer.html";
+            }else{
+                alert("Password incorrect");
+            }
+        }else{
+            alert("User details donot exist");
+        }
+    }
+    window.history.forward();
+    function noBack() {
+        window.history.forward();
+    }   
